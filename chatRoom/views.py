7a78e001d7b9d428326f347661736d514a9fabd0
Marc Proux <prouxmarc@gmail.com>
2023-01-08 19:33:33 +0100

    MAJ HTML

--- chatRoom/views.py
+++ chatRoom/views.py
@@ -3,5 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('chatRoom/index.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'chatRoom/index.html')
+
+def login(request):
+    return render(request, 'chatRoom/login.html')
