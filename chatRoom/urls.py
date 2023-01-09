f4b4f4bd7f069625db10c7f1bffd18e17deea65d
Marc Proux <prouxmarc@gmail.com>
2023-01-09 22:03:42 +0100

    login and signup

--- chatRoom/urls.py
+++ chatRoom/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('chatrooms', views.chatRooms, name='chatRooms'),
     path('signup', views.signup, name='signup'),
 ]
