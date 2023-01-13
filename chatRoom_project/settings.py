1e9f1635f0a11e1e96a05eb281cc10c0d5990b4c
Marc Proux <prouxmarc@gmail.com>
2023-01-13 18:18:53 +0100

    static

--- chatRoom_project/settings.py
+++ chatRoom_project/settings.py
@@ -114,9 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
